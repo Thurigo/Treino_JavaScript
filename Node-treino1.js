b4713ba18462a01049bb3fe15587ed784e4df3e9
Thurigo <arthur.spier@gmail.com>
2024-01-08 14:49:57 -0300

    Teste simples apenas para formação do curso da Alura

---
+++ Node-treino1.js
@@ -0,0 +1,37 @@
+const Numero = 1; 
+
+const PrimeiroNumero = 1; 
+const segundoNUmero = 2;
+
+const soma = PrimeiroNumero + segundoNUmero;
+
+console.log(soma)
+
+const Float = 3.3;
+const float2 = .5;
+
+const operaçao = Float * float2;
+
+console.log(operaçao);
+
+const Teste = "Teste"
+
+console.log(Teste + operaçao);
+
+
+//String
+
+const teste2 = "tamo junto"
+
+const tese1 = "Olá "
+
+console.log(tese1 + teste2)
+
+
+
+const rodar = 1 
+
+console.log(rodar === 1);
+
+console.log(rodar === 2);
+
